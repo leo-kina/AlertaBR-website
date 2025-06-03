b4e341276ff5e02072c65aa66dcf69e982fbd556
Leonardo Eiji Kina <f1boy.leokina@gmail.com>
2025-06-03 17:58:13 -0300

    fim menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Parallax
   const title = document.querySelector(".hero h2");
   const img = document.querySelector(".parallax-container img");
 
   window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
-    let offset;
-    if (window.innerWidth > 768) {
-      offset = scrollY * 0.5;
-    } else {
-      offset = scrollY * 0.15; 
-    }
+    const offset = window.innerWidth > 768 ? scrollY * 0.5 : scrollY * 0.15;
     title.style.transform = `translateX(${-offset}px)`;
     img.style.transform = `translateY(${scrollY * 0.2}px)`;
+  });
+
+  // Menu hamburguer
+ const hamburguerBtn = document.getElementById("hamburguerBtn");
+const navContent = document.querySelector(".navContent");
+
+hamburguerBtn.addEventListener("click", () => {
+  navContent.classList.toggle("active");
+  hamburguerBtn.classList.toggle("active"); // se quiser animar o botão também
+});
+
+
+  navContent.addEventListener("click", (e) => {
+    if (e.target.classList.contains("item-menu")) {
+      navContent.classList.remove("active");
+      hamburguerBtn.classList.remove("active");
+    }
   });
 });
